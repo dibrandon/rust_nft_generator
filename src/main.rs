bbb2c0add22503cac41059c26ec601067a324854
alejandro <diazbrandonalejandro@gmail.com>
2023-03-18 05:50:18 +0100

    first config done

--- src/main.rs
+++ src/main.rs
@@ -27,19 +27,33 @@ fn main() {
     let start_time = std::time::Instant::now();
     let layers = vec![
         Layer::new(
-            String::from("Backgrounds"),
+            String::from("Background"),
             String::from("layers/Background"),
         ),
         Layer::new(
-            String::from("Bottom Lid"),
-            String::from("layers/Bottom lid"),
+            String::from("Background Touch"),
+            String::from("layers/Background Touch"),
         ),
-        Layer::new(String::from("Eye Color"), String::from("layers/Eye color")),
-        Layer::new(String::from("Eyeball"), String::from("layers/Eyeball")),
-        Layer::new(String::from("Goo"), String::from("layers/Goo")),
-        Layer::new(String::from("Iris"), String::from("layers/Iris")),
-        Layer::new(String::from("Shine"), String::from("layers/Shine")),
-        Layer::new(String::from("Top Lid"), String::from("layers/Top lid")),
+        Layer::new(String::from("Color Filter A"), String::from("layers/Color Filter A")),
+        Layer::new(String::from("Bereichit Base"), String::from("layers/Bereichit Base")),
+        Layer::new(String::from("Bereichit Touch A"), String::from("layers/Bereichit Touch A")),
+        Layer::new(String::from("Color Filter B"), String::from("layers/Color Filter B")),
+        Layer::new(String::from("Element A"), String::from("layers/Element A")),
+        Layer::new(String::from("Barbed 1H"), String::from("layers/Barbed 1H")),
+        Layer::new(String::from("Barbed 2H"), String::from("layers/Barbed 2H")),
+        Layer::new(String::from("Barbed 3H"), String::from("layers/Barbed 3H")),
+        Layer::new(String::from("Barbed 4H"), String::from("layers/Barbed 4H")),
+        Layer::new(String::from("Barbed 5H"), String::from("layers/Barbed 5H")),
+        Layer::new(String::from("Barbed 6H"), String::from("layers/Barbed 6H")),
+        Layer::new(String::from("Barbed 7H"), String::from("layers/Barbed 7H")),
+        Layer::new(String::from("Barbed 8H"), String::from("layers/Barbed 8H")),
+        Layer::new(String::from("Barbed 9H"), String::from("layers/Barbed 9H")),
+        Layer::new(String::from("Barbed 10H"), String::from("layers/Barbed 10H")),
+        Layer::new(String::from("Barbed 11H"), String::from("layers/Barbed 11H")),
+        Layer::new(String::from("Barbed 12H"), String::from("layers/Barbed 12H")),
+        Layer::new(String::from("Element B"), String::from("layers/Element B")),
+        Layer::new(String::from("Dot"), String::from("layers/Dot")),
+        Layer::new(String::from("Signature"), String::from("layers/Signature")),
     ];
 
     let my_gen = Generator::new(
@@ -55,6 +69,7 @@ fn main() {
     let duration = end_time.duration_since(start_time);
     println!(
         "Time taken to generate {} images: {:?}",
-        END_TOKEN_ID - START_TOKEN_ID, duration
+        END_TOKEN_ID - START_TOKEN_ID,
+        duration
     );
 }
